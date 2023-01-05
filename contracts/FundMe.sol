9fe5014fe1a4f970ebbb0abe53eecf563ee8955d
Jimmy Hansard <jimmy.hansard@hotmail.com>
2023-01-05 09:18:56 +0800

    FundMe.sol updated.

--- contracts/FundMe.sol
+++ contracts/FundMe.sol
@@ -2,4 +2,26 @@
 
 pragma solidity ^0.8.9;
 
-contract FundMe {}
+error NotOwner();
+
+contract FundMe {
+    address public owner;
+    mapping(address => uint256) public addressToAmountFunded;
+    address[] public funders;
+
+    constructor() {
+        owner = msg.sender;
+    }
+
+    function fund() public payable {
+        addressToAmountFunded[msg.sender] = msg.value;
+        funders.push(msg.sender);
+    }
+
+    modifier ownerOnly() {
+        if (msg.sender != owner) revert NotOwner();
+        _;
+    }
+
+    function withdraw() public ownerOnly {}
+}
